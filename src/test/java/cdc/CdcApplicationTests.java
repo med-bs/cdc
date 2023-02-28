cd3b5cb170c5e042c17b99895aef331b532a4b29
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-02-28 23:43:30 +0100

    crud unit test valid

--- src/test/java/cdc/CdcApplicationTests.java
+++ src/test/java/cdc/CdcApplicationTests.java
@@ -1,13 +1,54 @@
 package cdc;
 
+import cdc.entitie.Transaction;
+import cdc.repository.TransactionRepository;
+import cdc.service.TransactionServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CdcApplicationTests {
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private TransactionServiceImpl tt;
+
     @Test
     void contextLoads() {
     }
+    @Test
+    public void testCreatTransaction(){
+        Transaction trans=new Transaction(100,"orig",120,130,"dest",200,220);
+        tt.saveTransaction(trans);
+    }
 
+    @Test
+    public void testFindTran(){
+        Transaction trans= transactionRepository.findById(2).get();
+        System.out.println(trans);
+    }
+    @Test
+    public void testUpdateTransaction(){
+        Transaction trans= transactionRepository.findById(1).get();
+        trans.setAmount(1100);
+        transactionRepository.save(trans);
+        System.out.println(trans);
+    }
+
+    @Test
+    public void testDeletTrensaction(){
+        transactionRepository.deleteById(130);
+    }
+
+    @Test
+    public void testFindAllTransactions(){
+        List<Transaction> transactionList= transactionRepository.findAll();
+        for (Transaction trans:transactionList) {
+            System.out.println(trans);
+        }
+    }
 }
