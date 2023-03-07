d332739525aae5921fb4f767ed658b9146565a47
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-07 15:17:15 +0100

    add Transaction ( ui + controller )

--- src/main/java/cdc/controller/TransactionUiContraller.java
+++ src/main/java/cdc/controller/TransactionUiContraller.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -31,4 +33,18 @@ public class TransactionUiContraller {
 
         return "index";
     }
+
+    @GetMapping("/newtransaction")
+    public String viewNewTransactionForm(Model model){
+        Transaction transaction = new Transaction();
+        model.addAttribute("transaction",transaction);
+        return "addTransaction";
+    }
+
+    @PostMapping("/newtransaction")
+    public String addNewTransaction(@ModelAttribute("transaction") Transaction transaction){
+        transactionService.saveTransaction(transaction);
+        return "redirect:/list/1";
+    }
+
 }
